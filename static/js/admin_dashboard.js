5943f34ec347b7019aa0476145742971b5c09b6a
mesoud5 <mesoudtemam5@gmail.com>
2024-04-27 07:35:40 -0700

    commit

---
+++ static/js/admin_dashboard.js
@@ -0,0 +1,15 @@
+// script.js
+
+// Function to update the date in the header
+function updateDate() {
+    const currentDate = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+    document.querySelector('.date').textContent = formattedDate;
+}
+
+// Call the function initially to set the date
+updateDate();
+
+// Update the date every second
+setInterval(updateDate, 1000);
